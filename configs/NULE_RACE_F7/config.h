90451fee80940693acfe395b2423b2caf36bf67e
kk <oskars@nule.lv>
2025-09-03 01:41:05 +0300

    Added NULE_RACE FC test target

---
+++ configs/NULE_RACE_F7/config.h
@@ -0,0 +1,116 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define FC_TARGET_MCU     STM32F7X2
+
+#define BOARD_NAME        NULE_RACE_F7
+#define MANUFACTURER_ID   NULE
+
+#define USE_GYRO
+#define USE_GYRO_SPI_ICM42688P
+#define USE_ACC
+#define USE_ACC_SPI_ICM42688P
+#define USE_BARO
+#define USE_BARO_DPS310
+#define USE_MAX7456
+#define USE_FLASH
+#define USE_FLASH_M25P16
+
+#define MOTOR1_PIN           PB7
+#define MOTOR2_PIN           PB6
+#define MOTOR3_PIN           PB5
+#define MOTOR4_PIN           PB4
+
+#define UART1_TX_PIN         PA9
+#define UART3_TX_PIN         PB10
+#define UART4_TX_PIN         PA0
+#define UART5_TX_PIN         PC12
+
+#define UART1_RX_PIN         PA10
+#define UART3_RX_PIN         PB11
+#define UART4_RX_PIN         PA1
+#define UART5_RX_PIN         PD2
+
+#define I2C1_SCL_PIN         PB8
+#define I2C1_SDA_PIN         PB9
+
+#define LED0_PIN             PC9
+#define LED_STRIP_PIN        PA8
+
+#define SPI1_SCK_PIN         PA5     A05
+#define SPI2_SCK_PIN         PB13    B13
+#define SPI3_SCK_PIN         PC10    C10
+#define SPI1_SDI_PIN         PA6     A06
+#define SPI2_SDI_PIN         PB14    B14
+#define SPI3_SDI_PIN         PC11    C11
+#define SPI1_SDO_PIN         PA7     A07
+#define SPI2_SDO_PIN         PB15    B15
+#define SPI3_SDO_PIN         PB2     B02
+
+#define ADC_VBAT_PIN         PC2
+#define ADC_CURR_PIN         PC3
+#define PINIO1_PIN           PA2
+#define PINIO2_PIN           PC8
+#define PINIO3_PIN           PB0
+#define FLASH_CS_PIN         PA15
+#define MAX7456_SPI_CS_PIN   PB12
+#define GYRO_1_EXTI_PIN      PC4
+#define GYRO_1_CS_PIN        PA4
+
+#define TIMER_PIN_MAPPING \
+    TIMER_PIN_MAP( 1, PB7 , 2,  0) \
+    TIMER_PIN_MAP( 2, PB6 , 2,  0) \
+    TIMER_PIN_MAP( 3, PB5 , 1,  0) \
+    TIMER_PIN_MAP( 4, PB4 , 1,  0) \
+    TIMER_PIN_MAP( 9, PA8 , 1,  0)
+#define ADC1_DMA_OPT            0
+
+#define USE_BARO
+#define BARO_I2C_INSTANCE           I2CDEV_1
+#define MAG_I2C_INSTANCE            I2CDEV_1
+
+#define DEFAULT_BLACKBOX_DEVICE     BLACKBOX_DEVICE_FLASH
+#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
+#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+#define DEFAULT_CURRENT_METER_SCALE 250
+#define MAX7456_SPI_INSTANCE SPI2
+
+#define SERIALRX_PROVIDER   SERIALRX_CRSF
+#define SERIALRX_UART       SERIAL_PORT_USART1
+
+#define FLASH_SPI_INSTANCE SPI3
+#define GYRO_1_SPI_INSTANCE SPI1
+
+#define BOX_USER1_NAME "VTX"
+#define PINIO1_BOX 40
+#define PINIO1_CONFIG 129
+#define BOX_USER2_NAME "LED PIN 2"
+#define PINIO2_BOX 41
+#define PINIO2_CONFIG 129
+#define BOX_USER3_NAME "LED PIN 3"
+#define PINIO3_BOX 42
+#define PINIO3_CONFIG 129
+#define BOX_USER4_NAME "LED PIN 4"
+#define PINIO4_BOX 43
+#define PINIO4_CONFIG 129
+
